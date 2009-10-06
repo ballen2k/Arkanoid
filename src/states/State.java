42d5f8747275a76d639bb99b16a5158af5bebffb
Robert Norlander <Robert Norlander@192.168.0.199>
2009-10-06 21:29:31 +0200

    Kommenterat State

--- src/states/State.java
+++ src/states/State.java
@@ -5,13 +5,43 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 
 public class State {
+	/** 
+	 * @param g Graphics-object that will be used to paint 
+	 * @param objectList List of objects that will be painted
+	 * @param userData Userdata to get score, life etc
+	 */
 	public void draw(Graphics g, ArrayList<controller.GameObject> objectList, model.UserData userData) { }
+	
+	/**
+	 * @param gameData Gamedata-instance to be able to retrieve info about the game
+	 * @param userData Userdata to get score, life etc
+	 */
 	public void update(model.GameData gameData, model.UserData userData) { }
+	
+	/**
+	 * @param pos_x X-position
+	 * @param pos_y Y-position 
+	 */
 	public void setMouse(int pos_x, int pos_y) { }
+	
+	/**
+	 * @param pos_x X-position
+	 * @param pos_y Y-position 
+	 */
 	public void setClick(int pos_x, int pos_y) { }
+	
+	/**
+	 * @param pos_x X-position
+	 * @param pos_y Y-position 
+	 */
 	public void setRightClick(int pos_x, int pos_y)  { }
 	public Color getColor(){return Color.gray;}
+	
+	/**
+	 * @return Returns the object that the state wants the game to paint
+	 */
 	public ArrayList<controller.GameObject> getObjects() { 
 		return new ArrayList<controller.GameObject>();
 	}
+
 }
